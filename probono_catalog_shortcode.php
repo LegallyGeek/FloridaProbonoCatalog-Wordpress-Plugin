02747524dba358a31338d338f8c01b7dc6bc54fb
James Haggard <james.haggard@savvysuit.io>
2017-01-03 11:54:19 -0500

    Updated plugin to use CDN for static assets.

--- probono_catalog_shortcode.php
+++ probono_catalog_shortcode.php
@@ -4,7 +4,7 @@
 Plugin Name: Florida Probono Catalog Shortcode
 Plugin URI: https://storefront.wiedza.works
 Description: A plugin that provides a shortcode that can insert the catalog in any page or post.
-Version: 0.1 BETA
+Version: 0.2 BETA
 Author: Wiedza Creations LLC
 Author URI: http://wiedza.works
 */
@@ -13,9 +13,9 @@ Author URI: http://wiedza.works
 function probono_catalog_func() {
 
 	// Return custom embed code
-	return  '<script src="https://storefront.wiedza.works/webcomponents-lite.min.js"></script>' . 
+	return  '<script src="https://flpbsf-assets.azureedge.net/webcomponents-lite.min.js"></script>' . 
 					"\n" .
-					'<link rel="import" href="https://storefront.wiedza.works/probono-catalog.html">' . 
+					'<link rel="import" href="https://flpbsf-assets.azureedge.net/probono-catalog.html">' . 
 					"\n" .
 					'<probono-catalog></probono-catalog>';
 
