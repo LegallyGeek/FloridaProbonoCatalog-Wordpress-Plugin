089a711497c987365461fd8b50629ffcb6c893d9
James Haggard <enquiries@jameshaggard.com>
2019-03-04 10:22:39 -0500

    Added parameter for "dark mode".

--- probono_catalog_shortcode.php
+++ probono_catalog_shortcode.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-Plugin Name: Florida Probono Catalog Shortcode
+Plugin Name: SavvySuit Pro Bono Catalog Shortcode
 Plugin URI: https://probono.savvysuit.com
 Description: A plugin that provides a shortcode that can insert the catalog in any page or post.
-Version: 0.3 BETA
+Version: 0.4 BETA
 Author: Wiedza Creations LLC
 Author URI: https://savvysuit.com
 */
@@ -17,13 +17,14 @@ function probono_catalog_func( $atts ) {
 		array(
       'partner_id' => '-1',
       'cohort_id' => '0',
+      'dark_mode' => 'false',
 		),
 		$atts,
 		'probono_catalog'
 	);
 
 	// Return custom embed code
-	return  '<probono-catalog cohort-id="' . $atts['cohort_id'] . '" partner-id="' . $atts['partner_id'] . '"></probono-catalog>' . 
+	return  '<probono-catalog cohort-id="' . $atts['cohort_id'] . '" partner-id="' . $atts['partner_id'] . '" dark-mode="' . $atts['dark_mode'] . '"></probono-catalog>' . 
 					"\n" .
 					'<script src="https://probono.savvysuit.com/cdn-origin/current/webcomponents-loader.js"></script>' . 
           "\n" .
